4aacff4d2397fe381e99ce718bd3727201c7326c
Chris <clombardi@problemsolutions.net>
2013-05-28 12:40:47 -0400

    Revised

---
+++ Forum/Controllers/PostsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Forum.Models;
+
+namespace Forum.Controllers
+{
+    public class PostsController : Controller
+    {
+        private PostDBContext db = new PostDBContext();
+
+        //
+        // GET: /Posts/
+
+        public ActionResult Index()
+        {
+            return View(db.Posts.ToList());
+        }
+
+        //
+        // GET: /Posts/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // GET: /Posts/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Posts/Create
+
+        [HttpPost]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(post);
+        }
+
+        //
+        // GET: /Posts/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // POST: /Posts/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        //
+        // GET: /Posts/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // POST: /Posts/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Post post = db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- Forum/Models/Post.cs
+++ Forum/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,14 @@ namespace Forum.Models
 {
     public class Post
     {
+        public int PostID { get; set; }
         public string PostTitle { get; set; }
         public string PostText { get; set; }
-        public int ThreadId { get; set; }
     }
+
+    public class PostDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+
 }
